73ec2c3ead389fefbc870566263daab8b6e56c4e
Najd404 <166329914+Najd404@users.noreply.github.com>
2024-04-12 03:50:18 +0300

    Giving Up Is Not An Option

---
+++ src/java/library/JavaLibrary.java
@@ -0,0 +1,15 @@
+package java.library;
+/**
+ *
+ * @author najdq
+ */
+public class JavaLibrary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
